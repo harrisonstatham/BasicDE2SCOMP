1382c0b43dabe0d3dd783f056fb4e7039071062b
Harrison Statham <harrison.statham@comcast.net>
2016-12-01 14:10:11 -0500

    File compiles properly

--- FPGASimpleComputer/scomp.vhd
+++ FPGASimpleComputer/scomp.vhd
@@ -93,7 +93,7 @@ ARCHITECTURE a OF SCOMP IS
 		wrcontrol_aclr_a => "NONE",
 		address_aclr_a   => "NONE",
 		outdata_aclr_a   => "NONE",
-		init_file        => "SimpleRobotProgram.mif",
+		init_file        => "example.mif",
 		lpm_hint         => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type         => "altsyncram"
 	)
